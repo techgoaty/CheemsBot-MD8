4b5cd757e8d2cfdb19f1ef6dcaa46c6e61b127a3
techgoaty <153655883+techgoaty@users.noreply.github.com>
2024-02-14 15:24:18 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['2348118820641'] //ur owner number
+global.ownernomer = "2348118820641" //ur owner number2
+global.ownername = "Anonymous🦄" //ur owner name
+global.ytname = "techgoaty" //ur yt chanel name
+global.socialm = "GitHub: techgoaty" //ur github or insta name
+global.location = "Nigeria" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "ANONYMIX"
+global.ownernumber = '2348118820641'
+global.ownername = 'ANONYMOUS🎭'
+global.ownerNumber = ["2348118820641@s.whatsapp.net"]
+global.ownerweb = "https://www.tiktok.com/@techgoat?_t=8jsUkajJDN8&_r=1"
+global.websitex = "https://www.tiktok.com/@techgoat?_t=8jsUkajJDN8&_r=1"
+global.wagc = "https://chat.whatsapp.com/B6hifgu7KbF0Y6hRg0mJAp"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wm = "TECHGOAT📉"
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "ANONYMOUS🦄\n\n"
+global.creator = "2348118820641@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
